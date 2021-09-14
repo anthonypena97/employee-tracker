69cfffa123a534b44425f9e5577d7684e7c73fdb
Anthony Pena <apena5@alumni.jh.edu>
2021-09-14 03:13:16 -0400

    completed view employees function

--- db/schema.sql
+++ db/schema.sql
@@ -4,22 +4,25 @@ USE employee_tracker;
 
 CREATE TABLE departments (
   id INTEGER AUTO_INCREMENT PRIMARY KEY,
-  dept_name VARCHAR(50) NOT NULL
+  department_name VARCHAR(30) NOT NULL,
+  UNIQUE (department_name)
 );
 
 CREATE TABLE roles (
   id INTEGER AUTO_INCREMENT PRIMARY KEY,
-  title VARCHAR(30) NOT NULL
+  title VARCHAR(30) NOT NULL,
+  department INTEGER NOT NULL,
+  salary DECIMAL(9,2),
+  CONSTRAINT fk_department FOREIGN KEY (department) REFERENCES departments(id) ON DELETE CASCADE,
+  UNIQUE (title)
 );
 
-CREATE TABLE employees (
+CREATE TABLE employees(
   id INTEGER AUTO_INCREMENT PRIMARY KEY,
   first_name VARCHAR(30) NOT NULL,
   last_name VARCHAR(30) NOT NULL,
   role_title INTEGER NOT NULL,
-  department INTEGER NOT NULL,
-  salary INTEGER NOT NULL,
-  manager VARCHAR(50) NOT NULL,
-  CONSTRAINT fk_department FOREIGN KEY (department) REFERENCES departments(id) ON DELETE CASCADE,
-  CONSTRAINT fk_role FOREIGN KEY (role_title) REFERENCES roles(id) ON DELETE CASCADE
+  manager_id INTEGER,
+  CONSTRAINT fk_role FOREIGN KEY (role_title) REFERENCES roles(id) ON DELETE CASCADE,
+  CONSTRAINT fk_manager FOREIGN KEY (manager_id) REFERENCES employees(id) ON DELETE CASCADE
 );
